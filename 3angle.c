e6f137369b8bb4aefd668f0b7cdb7033ff1cf486
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-05 02:44:32 -0800

    3angle.c
    progeram

---
+++ 3angle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int angle[3],i;
+char radius,lenght,width;
+printf("enter the vlaue in radius");
+scanf("%d",&redius);
+printf("enter the vlaue in lenght");
+scanf("%d",&lenght);
+printf("enter the vlaue in width");
+scanf("%d",&width);
+for(i=0;i<3;i++)
+{
+printf("the interior angle of a triangle");
+scanf("%d",&triangle);
+triangle=(radius*lenght+width);
+getch();
+}
