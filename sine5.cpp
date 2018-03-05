318e224a19c7de1b157f40cc6a33736dc33e8bfe
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-05 02:31:14 -0800

    sine5.cpp

---
+++ sine5.cpp
@@ -0,0 +1,22 @@
+ #include<iostream.h>
+ #include<conio.h>
+ #include<math.h>
+
+ main()
+    {
+       clrscr();
+
+       int x;
+
+       floatangle;
+
+       cout<<"\n Enter the value of x = ";
+       cin>>x;
+
+       amgle=sin(x);
+
+       cout<<" Sin ("<<x<<") = "<<value<<endl;
+
+       getch();
+       return 0;
+    }
