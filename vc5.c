97d916d8b660c9c6344eb1bc337c1b6459e97d99
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-05 03:10:55 -0800

    vc5.c
    program

--- vc5.c
+++ vc5.c
@@ -3,12 +3,10 @@
 #include<conio.h>
 void main()
 {
-    int i, n, d ;
+    int i, n,  ;
     printf("Enter the limit : ") ;
     scanf("%d", &n) ;
-    printf("\nEnter the number : ") ;
-    scanf("%d", &d) ;
-    printf("\nThe numbers divisible by %d are :\n\n", d) ;
+    printf("\nEnter the number : ") 
     for(i = 1 ; i <= n ; i++)
     if(i % d == 0)
     printf("%d\t", i) ;
