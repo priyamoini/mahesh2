e15d0e3236def130ea8236a55321e01b3a5e8f87
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-09 16:12:30 +0530

    s9.c
    program

---
+++ s9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+username string  
+void main()
+{
+int a[10],b[5];
+printf("enter the value of string:",a);
+scanf("%d",&a);
+printf("enter the value of string:",b);
+scanf("%d",&b); 
+for(i=0;i<=2;i=++);
+{
+printf("enter the string value");
+}
+return=0;
+}
