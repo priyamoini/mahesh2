c5560a8af1b02680169fa3bfbaf57f900c42417d
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-12 16:29:03 +0530

    str12.c
    program

---
+++ str12.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+char getSecondMostFreq(char *str)
+{
+    int count[NO_OF_CHARS] = {0}, i;
+    for (i=0; str[i]; i++)
+        (count[str[i]])++;
+    int first = 0, second = 0;
+    for (i = 0; i < NO_OF_CHARS; i++)
+    {
+        if (count[i] > count[first])
+        {
+            second = first;
+            first = i;
+        }
+        else if (count[i] > count[second] &&
+                 count[i] != count[first])
+            second = i;
+    }
+ 
+    return second;
+    }
+int main()
+{
+  char str[] = "geeksforgeeks";
+  char res = getSecondMostFreq(str);
+  if (res != '\0')
+     printf("Second most frequent char is %c", res);
+  else
+     printf("No second most frequent character");
+  return 0;
+}
