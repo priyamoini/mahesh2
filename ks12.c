64806e8b9176f4119ed6e78c5bef5040e2acb8ce
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-12 16:06:10 +0530

    ks12.c
    program

---
+++ ks12.c
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int(char str1[], char str2[], int m, int n)
+{
+    if (m == 0) return true;
+    if (n == 0) return false;
+    if (str1[m-1] == str2[n-1])
+        return isSubSequence(str1, str2, m-1, n-1);
+    return isSubSequence(str1, str2, m, n-1);
+}
+int main()
+{
+    char str1[] = "gksrek";
+    char str2[] = "geeksforgeeks";
+    int m = strlen(str1);
+    int n = strlen(str2);
+    isSubSequence(str1, str2, m, n)? cout << "Yes ":
+                                     cout << "No";
+    return 0;
+}
