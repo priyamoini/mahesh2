24fc448a20fdd06d9e6d12b4843b64e47762d6bd
priyamoini <35995884+priyamoini@users.noreply.github.com>
2018-03-12 15:39:13 +0530

    di.c
    program

--- di.c
+++ di.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    long long n;
-    int count = 0;
+    int  n;
+    int count = 3;
 
     printf("Enter an integer: ");
-    scanf("%lld", &n);
+    scanf("%d", &n);
 
     while(n != 0)
     {
          n = n/10
-        n = 10;
+        n = /10;
         ++count;
     }
 
     printf("Number of digits: %d", count);
+    return 0;
 }
